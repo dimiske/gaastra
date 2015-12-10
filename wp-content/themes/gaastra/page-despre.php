07c236888196eedd7fd8c47ad8098e8ec9e7e8bf
Alex Geogea <alexandra.geogea@gmail.com>
2015-12-10 15:52:08 +0200

    modificari pe pagina despre

---
+++ wp-content/themes/gaastra/page-despre.php
@@ -0,0 +1,200 @@
+
+<?php get_header(); ?>
+
+<!--<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/logo.png"/>-->
+<div class="row">
+	<div class="intro col-lg-12 col-sm-12 col-xs-12 p-a-0 m-l-0 m-r-0">
+		<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-banner.jpg" class="img-responsive"/>
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg-12 p-a-0 m-l-0 m-r-0 text-center">
+		<h2 class="padding-big">Funcţionalitatea întâlneşte designul</h2>
+		<p class="paragraph-big">GAASTRA este unul dintre liderii mondiali în materie de îmbrăcăminte de înaltă calitate, îmbinând perfect funcționalitatea cu moda. Concepută pentru bărbați, femei și copii, îmbrăcămintea GAASTRA urmărește liniile design-ului modern, păstrând la bază, prin materialele folosite și atenția la detaliu, elementele de utilitate.</p>
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg-12 p-a-0 m-l-0 m-r-0 text-center">
+		<h3 class="padding-medium">Proprietăţile hainelor Gaastra</h3>
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg-12 p-a-0 m-l-0 m-r-0">
+		<div class="container aria-section">
+			<div class="row">
+				<div class="col-sm-12 col-md-6 col-lg-6">
+					<figure>
+						<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-1.jpg" class="img-responsive">
+					</figure>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-6 text-center">
+					<div class="vertical-container">
+						<h3>Apă</h3>
+						<p class="paragraph-big">Materiale impermeabile, cusături sigilate.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12 col-md-6 col-lg-6 text-center">
+					<div class="vertical-container">
+						<h3>Vânt</h3>
+						<p class="paragraph-big">Materiale cu rezistenţă crescută la vânt.</p>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-6">
+					<figure>
+						<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-2.jpg" class="img-responsive">
+					</figure>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12 col-md-6 col-lg-6">
+					<figure>
+						<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-3.jpg" class="img-responsive">
+					</figure>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-6 text-center">
+					<div class="vertical-container">
+						<h3>Temperatură</h3>
+						<p class="paragraph-big">Materiale subţiri, cu transfer termic foarte mic.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 p-a-0 m-l-0 m-r-0 text-center">
+			<h1 class="padding-medium m-b-0">Istoria brandului Gaastra</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-6 col-lg-offset-3">
+			<figure>
+				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-4.jpg" class="img-responsive">
+			</figure>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-12 p-a-0 m-l-0 m-r-0 text-center">
+			<p class="paragraph-big">Istoria Gaastra a început în 1897, când Douwe Gaastra a pus bazele companiei sale de navigaţie în Seek, Olanda. Ambiţia lui a fost să facă navigaţia mai rapidă, competitivă şi eficientă. La scurt timp, Gaastra a devenit un nume comun în lumea globală de navigaţie.</p>
+		</div>
+	</div>
+	<div class="row padding-small">
+		<div class="col-sm-12 col-md-6 col-lg-6 m-l-0 m-r-0 text-center">
+			<figure>
+				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-5.jpg" class="img-responsive">
+			</figure>
+		</div>
+		<div class="col-sm-12 col-md-6 col-lg-6 m-l-0 m-r-0 text-center">
+			<p class="paragraph-big">Este o linie subţire între frică şi pasiune. Douwe Gaastra şi-a pierdut inima printre valuri, dedicându-se creeării celui mai bun echipament de navigaţie. A ştiut că o confruntare cu marea este despre respect şi a fost copleşit de dragoste pentru apele măreţe. Chiar şi în această zi noi respectăm, iubim şi urmărim moştenirea lăsată de Douwe.</p>
+		</div>
+	</div>
+</div>
+<div class="timeline-container">
+	<div class="container">
+		<div class="col-lg-12 p-a-0 m-l-0 text-center">
+			<h3 class="padding-medium">Gaastra de-a lungul anilor</h3>
+		</div>
+		<div class="row">
+			<div class="col-sm-12 col-md-8 col-lg-8 p-a-0 m-r-0 m-l-0 text-center">
+				<div class="padding-small">
+					<p class="years">1897</p>
+					<p class="paragraph-big">Înfiinţarea companiei de navigaţie de către Douwe Gaastra în Seek, Olanda.</p>
+				</div>
+				<div class="padding-small">
+					<p class="years">1970</p>
+					<p class="paragraph-big">Între 1970 şi 1990 Gaastra este recunoscut ca un brand prestigios de windsurfing.</p>
+				</div>
+				<div class="padding-small">
+					<p class="years">1996</p>
+					<p class="paragraph-big">Primele linii de articole de îmbrăcăminte - Gaastra Pro şi Gaastra Sportwear.</p>
+				</div>
+				<div class="padding-small">
+					<p class="years">2000</p>
+					<p class="paragraph-big">Gaastra devine parte a McGregor Fashion Group B.V., grup înfiinţat în anul 1993.</p>
+				</div>
+				<div class="padding-small">
+					<p class="years">2005</p>
+					<p class="paragraph-big">Gaastra devine sponsor al celui mai prestigios eveniment de navigaţie, şi anume Les Voiles de St. Tropez.</p>
+				</div>
+				<div class="padding-small">
+					<p class="years">2010</p>
+					<p class="paragraph-big">Gaastra ajunge în ţările Scandinave şi deschide primul magazin în Italia. În acelaşi an, Gaastra devine unul dintre principalii sponsori asociaţi cu SAIL 2010 şi începe un parteneriat de sponsorizare cu The Jethou, un iacht de curse.</p>
+				</div>
+				<div class="padding-small">
+					<p class="years">2011</p>
+					<p class="paragraph-big">Gaastra deschide primul magazin în SUA, în Newport, Rhode Island. Tot atunci, Gaastra devine sponsor oficial al Les Voiles de Saint Barth şi partener oficial al muzeului Scheepvaartmuseum.</p>
+				</div>
+				<div class="padding-small">
+					<p class="years">2012</p>
+					<p class="paragraph-big">Gaastra devine partener oficial al evenimentului internaţional B44 Barcelona, în golful Trieste din partea nordică a Mării Mediterane şi deschide primul său sediu central în Amsterdam, Olanda.</p>
+				</div>
+				<div class="padding-small">
+					<p class="years">2013</p>
+					<p class="paragraph-big">Pentru al patrulea an consecutiv, Gaastra este partener şi sponsor oficial de îmbrăcăminte pentru Copa del Rey.</p>
+				</div>
+				<div class="padding-small">
+					<p class="years">2014</p>
+					<p class="paragraph-big">Gaastra îşi deschide uşile primului magazin din România, în Cluj-Napoca.</p>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-4 col-lg-4 p-a-0 m-r-0">
+				<figure>
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-6.jpg" class="img-responsive">
+				</figure>
+				<figure>
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-7.jpg" class="img-responsive">
+				</figure>
+				<figure>
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-8.jpg" class="img-responsive">
+				</figure>
+				<figure>
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-9.jpg" class="img-responsive">
+				</figure>
+				<figure>
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-10.jpg" class="img-responsive">
+				</figure>
+				<figure>
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-11.jpg" class="img-responsive">
+				</figure>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="col-lg-12 p-a-0 m-l-0 m-r-0 text-center">
+		<h1 class="padding-medium">Gaastra astăzi</h1>
+	</div>	
+	<div class="row">
+		<div class="col-sm-12 col-md-4 col-lg-4 p-a-0 m-l-0 m-r-0 text-center module">
+			<h3>Partener pentru</h3>
+			<div class="circle">
+				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-14.jpg" class="img-responsive"/>
+				<span class="big-number">11</span>
+			</div>
+			<p>EVENIMENTE DE NAVIGAŢIE <br>peste tot în lume</p>
+		</div>
+		<div class="col-sm-12 col-md-4 col-lg-4 p-a-0 m-l-0 m-r-0 text-center module">
+			<h3>Sponsor pentru</h3>
+			<div class="circle">
+				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-13.jpg" class="img-responsive"/>
+				<span class="big-number">5</span>
+			</div>
+			<p>ECHIPE <br>din scena navigaţiei profesionale</p>
+		</div>
+		<div class="col-sm-12 col-md-4 col-lg-4 p-a-0 m-l-0 m-r-0 text-center module">
+			<h3>În mai mult de</h3>
+			<div class="circle">
+				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/despre/despre-image-12.png" class="img-responsive"/>
+				<span class="big-number">1.500</span>
+			</div>
+			<p>MAGAZINE <br>disponibile în întreaga lume</p>
+		</div>
+	</div>
+</div>
+
+
+
+<?php get_footer(); ?>
