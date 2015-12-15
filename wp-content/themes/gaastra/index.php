0e7c2055fa745bcae763a6e6b707224a7904f0de
dimiske <misi.veres@gmail.com>
2015-12-15 15:48:47 +0200

    eol

--- wp-content/themes/gaastra/index.php
+++ wp-content/themes/gaastra/index.php
@@ -1,1 +1,23 @@
-<?php get_header(); ?><div class="clearfix"></div><?phpif(is_front_page()){    get_template_part('content', 'front');}else{    if(have_posts()){        // Start the Loop.        while(have_posts()){            the_post();            get_template_part('content', get_post_format());        }    }else{        get_template_part('content', 'index');    }}get_footer();
+<?php get_header(); ?>
+
+<div class="clearfix"></div>
+
+<?php
+if(is_front_page()){
+    get_template_part('content', 'front');
+}else{
+    if(have_posts()){
+        // Start the Loop.
+        while(have_posts()){
+            the_post();
+            get_template_part('content', get_post_format());
+        }
+    }else{
+        get_template_part('content', 'index');
+    }
+}
+
+get_footer();
+
+
+
