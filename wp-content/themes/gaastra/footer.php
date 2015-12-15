68c967336635a13cb7d20ea1dcf5094d8495da42
dimiske <misi.veres@gmail.com>
2015-12-15 15:52:43 +0200

    eols

--- wp-content/themes/gaastra/footer.php
+++ wp-content/themes/gaastra/footer.php
@@ -1,1 +1,30 @@
-    <div class="footer-gaastra p-a-0">        </div>    <!-- jQuery first, then Bootstrap JS. -->    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>    <!--<script src="js/main.js"></script>-->    <script>        jQuery('table').addClass('table');        jQuery('table td img').addClass('img-responsive');//        jQuery('a').click(function(){//            return false;//        });    </script>    <script>    $(document).ready(function() {    $('#Carousel').carousel({        interval: false,        pause: true    })});     </script>    <?php wp_footer(); ?></body></html>
+    <div class="footer-gaastra p-a-0">
+    
+    </div>
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <!--<script src="js/main.js"></script>-->
+    <script>
+        jQuery('table').addClass('table');
+        jQuery('table td img').addClass('img-responsive');
+//        jQuery('a').click(function(){
+//            return false;
+//        });
+    </script>
+    <script>
+    $(document).ready(function() {
+    $('#Carousel').carousel({
+        interval: false,
+        pause: true
+    })
+}); 
+    </script>
+    <?php wp_footer(); ?>
+
+</body>
+
+</html>
+
+
+
