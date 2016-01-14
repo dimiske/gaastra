ec161f8540b49d790ecdff7791e678e554092c87
Alex Geogea <alexandra.geogea@gmail.com>
2016-01-14 16:18:48 +0200

    Lucrat la footer

--- wp-content/themes/gaastra/footer.php
+++ wp-content/themes/gaastra/footer.php
@@ -1,33 +1,58 @@
     <div class="push"></div>
     </div><!-- wrapper end -->
     
-    <div class="footer-gaastra">
+    <div class="footer-gaastra lg">
         <div class="container">
             <div class="col-lg-5 col-md-5 footer-1">
                 <div><a href="<?php echo site_url(); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/footer/logo-footer.jpg" alt="" /></a></div>
                 <div class="footer-menu">
                     <span><a href="<?php echo site_url(); ?>/termeni-si-conditii">Termeni și condiții</a></span>
-                    <span><a href="<?php echo site_url(); ?>/contact">Contact</a></span>
-                    <span><a href="<?php echo site_url(); ?>/blog">Blog</a></span>
                 </div>
             </div>
             
-            <div class="col-lg-5 col-md-5 footer-2">
-                <div>Fii primul care află noutățile Gaastra</div>
-                <?php echo do_shortcode('[mc4wp_form id="255"]'); ?>
-                <!--<div><input name="email" placeholder="Adresa de e-mail" class="form-input email-address" /></div>
-                <div><input type="button" value="Abonează-te!" class="custom-button-footer"></div>-->
+            <div class="col-lg-4 col-md-4 footer-2">
+                <div class="label-input">Fii primul care află noutățile Gaastra!</div>
+                <!--<?php echo do_shortcode('[mc4wp_form id="255"]'); ?>-->
+               <input name="email" placeholder="Adresa de e-mail" class="form-input email-address" />
+                <input type="button" value="Abonează-te!" class="custom-button-footer">
             </div> 
             
-            <div class="col-lg-2 col-md-2 footer-3">
+            <div class="col-lg-3 col-md-3 footer-3">
                 <div>Urmărește-ne</div>
                 <div class="logos">
-                    <div class="fb"><a href="https://www.facebook.com/gaastra.ro" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/footer/logo-fb.jpg" alt="" /></a></div>
-                    <div class="pint"><a href="https://www.pinterest.com/gaastraromania/" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/footer/logo-pint.jpg" alt="" /></a></div>
-                    <div class="insta"><a href="https://www.instagram.com/gaastra_romania/" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/footer/logo-insta.jpg" alt="" /></a></div>
+                    <div class="fb"><a href="https://www.facebook.com/gaastra.ro" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/icons/facebook-icon.png" alt="" /></a></div>
+                    <div class="pint"><a href="https://www.pinterest.com/gaastraromania/" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/icons/pinterest-icon.png" alt="" /></a></div>
+                    <div class="insta"><a href="https://www.instagram.com/gaastra_romania/" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/icons/instagram-icon.png" alt="" /></a></div>
                 </div>
-                <div class="copyright">Gaastra &copy; <?php echo date('Y'); ?></br>Dezvoltat de <a href="http://fortech.ro" target="_blank">Fortech</a></div>
+                <div class="copyright">Gaastra &copy; <?php echo date('Y'); ?></br>Dezvoltat de <a href="http://fortech.ro" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/footer/fortech-logo.png"  height="10" /></a></div>
             </div>
+        </div>
+    </div>
+    
+    <div class="footer-gaastra sm">
+        <div class="container">
+            <div class="col-xs-12 col-sm-7 footer-1">
+                <div class="logo"><a href="<?php echo site_url(); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/footer/logo-footer.jpg" alt="" /></a></div>
+                <div class="footer-menu">
+                    <span><a href="<?php echo site_url(); ?>/termeni-si-conditii">Termeni și condiții</a></span>
+                </div>
+            </div>
+            
+            <div class="col-xs-12 col-sm-5 footer-3">
+                <div class="logos">
+                    <a href="https://www.facebook.com/gaastra.ro" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/icons/facebook-icon.png" alt="" /></a>
+                    <a href="https://www.pinterest.com/gaastraromania/" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/icons/pinterest-icon.png" alt="" /></a>
+                    <a href="https://www.instagram.com/gaastra_romania/" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/icons/instagram-icon.png" alt="" /></a>
+                </div>
+            </div>
+            
+            <div class="col-xs-12 col-sm-12 footer-2">
+                <div class="label-input">Fii primul care află noutățile Gaastra!</div>
+                <!--<?php echo do_shortcode('[mc4wp_form id="255"]'); ?>-->
+                <input name="email" placeholder="Adresa de e-mail" class="form-input email-address" />
+                <input type="button" value="Abonează-te!" class="custom-button-footer">
+                <div class="copyright">Gaastra &copy; <?php echo date('Y'); ?></br>Dezvoltat de <a href="http://fortech.ro" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/footer/fortech-logo.png"  height="10" /></a></div>
+            </div> 
         </div>
     </div>
 
@@ -41,17 +66,16 @@
         jQuery('table td img, article.gaastrablog .entry_content img').addClass('img-responsive');
         jQuery('.blogpost-item img').addClass('img-responsive img-fluid');
         jQuery('.blogpost-item.pagination .page-numbers, .gaastra-blog #submit').addClass('button-link');
-//        jQuery('a').click(function(){
-//            return false;
-//        });
+        jQuery('.form-group input, .form-group textarea').addClass('form-input');
+        jQuery('.form-group input#submit').removeClass('form-input');
     </script>
     <script>
-    $jQuery(document).ready(function() {
-    $jQuery('.carousel.slide').carousel({
-        interval: false,
-        pause: true
-    })
-}); 
+        $jQuery(document).ready(function() {
+            $jQuery('.carousel.slide').carousel({
+                interval: false,
+                pause: true
+            })
+        }); 
     </script>
     <?php wp_footer(); ?>
 
